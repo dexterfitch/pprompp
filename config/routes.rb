0534939b2b663fac87c7dc16b29e68f2567dcab2
Dex Fitch <fitch.dexter@gmail.com>
2020-11-10 16:28:49 -0800

    Made routes DRYer, added validations to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :characters
-  resources :goals
-  resources :motivations
-  resources :tactics
-  resources :prompts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :characters, :goals, :motivations, :tactics, :prompts
 end
