e879c37946eab584bb13abf421acdc0c06321514
Dex Fitch <fitch.dexter@gmail.com>
2020-11-12 13:32:17 -0800

    Flesh out nav, begin setting up character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,21 @@
 class CharactersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @characters = Character.all
   end
 
   def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render :new
+    end
   end
 
   def new
+    @character = Character.new
   end
 
   def edit
@@ -20,6 +28,12 @@ class CharactersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :species, :race, :age, :average_lifespan, :physical_description, :notes, :shared)
   end
 
 end
