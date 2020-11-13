f9961096c5c2da62339acbad25d68129de7163f8
Dex Fitch <fitch.dexter@gmail.com>
2020-11-13 15:18:21 -0800

    Build out views for Goals, Motivations, and Tactics.

--- app/controllers/motivations_controller.rb
+++ app/controllers/motivations_controller.rb
@@ -1,25 +1,55 @@
 class MotivationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_motivation_variable, only: [:edit, :update, :destroy]
 
   def index
+    @motivations = Motivation.all
+  end
+
+  def index_mine
+    @motivations = current_user.created_motivations.all
   end
 
   def create
+    @motivation = Motivation.new(motivation_params)
+    if @motivation.save
+      redirect_to motivation_path(@motivation)
+    else
+      render :new
+    end
   end
 
   def new
+    @motivation = Motivation.new
   end
 
   def edit
   end
 
   def show
+    @motivation = Motivation.find(params[:id])
   end
 
   def update
+    if @motivation.update(motivation_params)
+      redirect_to motivation_path(@motivation)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @motivation.destroy
+    redirect_to motivation_index_mine_path(current_user)
   end
 
+  private
+
+  def motivation_params
+    params.require(:motivation).permit(:user_id, :description, :notes, :shared)
+  end
+
+  def set_motivation_variable
+    @motivation = current_user.created_motivations.find(params[:id])
+  end
 end
--- app/controllers/tactics_controller.rb
+++ app/controllers/tactics_controller.rb
@@ -1,25 +1,55 @@
 class TacticsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tactic_variable, only: [:edit, :update, :destroy]
 
   def index
+    @tactics = Tactic.all
+  end
+
+  def index_mine
+    @tactics = current_user.created_tactics.all
   end
 
   def create
+    @tactic = Tactic.new(tactic_params)
+    if @tactic.save
+      redirect_to tactic_path(@tactic)
+    else
+      render :new
+    end
   end
 
   def new
+    @tactic = Tactic.new
   end
 
   def edit
   end
 
   def show
+    @tactic = Tactic.find(params[:id])
   end
 
   def update
+    if @tactic.update(tactic_params)
+      redirect_to tactic_path(@tactic)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @tactic.destroy
+    redirect_to tactic_index_mine_path(current_user)
   end
 
+  private
+
+  def tactic_params
+    params.require(:tactic).permit(:user_id, :description, :notes, :shared)
+  end
+
+  def set_tactic_variable
+    @tactic = current_user.created_tactics.find(params[:id])
+  end
 end
