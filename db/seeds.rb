4e6f7542c92e56e6537013c6cadf944cc2410827
Dex Fitch <fitch.dexter@gmail.com>
2020-11-14 16:31:16 -0800

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Character.create(name: "Globfinne", species: "Goblin", race: "Merfolk", age: 23, average_lifespan: 148, physical_description: "Small, green, with a yellowfin tail.",  notes: "Perhaps they tend to use shipwrecks as their lair?", shared: true)
+Character.create(name: "Chitta", species: "Mouseguard", race: "Field", age: 15, average_lifespan: 50, physical_description: "About 51cm tall, with a bobbed tail, and brown fur. Walks upright unless going at top speed, then switches to all fours.", notes: "(Shoutout to Reepicheep)", shared: "true")
+Character.create(name: "Lissk", species: "Leafen", race: "Wisteria", age: 327, average_lifespan: 1000, physical_description: "Tall, graceful, but kinda messy.", notes: "Loves mechanical time pieces, especially cuckoo clocks.", shared: true)
+Character.create(name: "B'tar", species: "Yellow-gem", race: "Citrinian", age: 200345, average_lifespan: nil, physical_description: "B'tar prefers four-legged canid forms, though they usually add horns and a skullplate to their form for extra defense and menacing appearance.", notes: "Formless, those of the many variate *-gem species can arrange their fluid crystal structure into any shape. (OK I know it's super Steven Universe, but... SU is awesome!)", shared: true)
 
-Character.create(name: "Gobzin", species: "Goblin", race: "Merfolk", age: 23, average_lifespan: 148, physical_description: "Small, green, with a yellowfin tail.",  notes: "Perhaps they tend to use shipwrecks as their lair?", shared: true)
-Character.create(name: "Nilfen", species: "Mouseguard", race: "Field", age: "15", average_lifespan: "50", physical_description: "About 50cm tall, with a bobbed tail, and brown fur. Walks upright unless going at top speed, then switches to all fours.", notes: "Shoutout to Reepicheep", shared: "true")
+Goal.create(description: "Discover a way to bottle lightning", notes: "(The Witcher, anyone?)", shared: true)
+Goal.create(description: "Travel a great distance", notes: "This could be for either fear or fun", shared: true)
+Goal.create(description: "Make perfect macarons", notes: "A loved one used to make them", shared: true)
+Goal.create(description: "Attain the Tri-force of Power", notes: "(This is considered fair-use, right???)", shared: true)
+
+Motivation.create(description: "Get revenge on the one who killed their dog", notes: "(Honestly, same.)", shared: true)
+Motivation.create(description: "Try every type of cuisine", notes: "A serious foodie!", shared: true)
+Motivation.create(description: "Become the greatest Yu-Gi-Oh duelist", notes: "Loves the card 'Baby Dragon'", shared: true)
+Motivation.create(description: "Heist the declaration of independence", notes: "But for a good reason", shared: true)
+
+Tactic.create(description: "Uses wealth to get their way", notes: "They're not really evil, just lazy and spoiled", shared: true)
+Tactic.create(description: "Punch first, ask questions later", notes: "They know kung-fu. (And judo).", shared: true)
+Tactic.create(description: "Big, warm hugs", notes: "With consent, of course", shared: true)
+Tactic.create(description: "Turn trash into treasure", notes: "Half raccoon/half MacGuyver", shared: true)
